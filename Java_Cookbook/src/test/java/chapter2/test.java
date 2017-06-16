c9b9090b0e0e34b1e760d852761c8fcab9638997
LK <307832539@qq.com>
2017-06-17 01:03:40 +0800

    =.=

--- Java_Cookbook/src/test/java/chapter2/test.java
+++ Java_Cookbook/src/test/java/chapter2/test.java
@@ -21,6 +21,7 @@ public class test {
     //获取系统属性信息
     public void getProperty() {
         Properties properties = System.getProperties();
-        System.out.println(properties.toString().replace(",", "\n"));
+//        System.out.println(properties.toString().replace(",", "\n"));//不知道有list(System.out)这种方法使用的笨蛋方法
+        properties.list(System.out);
     }
 }
---
+++ Java_Cookbook/src/test/java/chapter3/test.java
@@ -0,0 +1,7 @@
+package chapter3;
+
+/**
+ * Created by LK on 2017/6/17.
+ */
+public class test {
+}
