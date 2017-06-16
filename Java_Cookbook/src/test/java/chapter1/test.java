43ba03e41148bdfe9a4cb0ce8bed074d4a3a950e
LK <307832539@qq.com>
2017-06-17 00:41:34 +0800

    =.=

---
+++ Java_Cookbook/src/test/java/chapter1/test.java
@@ -0,0 +1,7 @@
+package chapter1;
+
+/**
+ * Created by LK on 2017/6/16.
+ */
+public class test {
+}
---
+++ Java_Cookbook/src/test/java/chapter2/test.java
@@ -0,0 +1,26 @@
+package chapter2;
+
+import org.junit.Test;
+
+import java.util.Properties;
+
+/**
+ * Created by LK on 2017/6/17.
+ */
+public class test {
+
+    @Test
+    //获取环境变量
+    public void getEnv() {
+        //Map<String,String> map =  System.getenv()
+        System.out.println(System.getenv());
+        System.out.println(System.getenv("PATH"));
+    }
+
+    @Test
+    //获取系统属性信息
+    public void getProperty() {
+        Properties properties = System.getProperties();
+        System.out.println(properties.toString().replace(",", "\n"));
+    }
+}
