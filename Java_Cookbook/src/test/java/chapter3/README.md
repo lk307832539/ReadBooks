370fba3ecd734ebe4cf73088c3a598818ffb4d65
LK <307832539@qq.com>
2017-06-17 23:04:49 +0800

    =.=

--- Java_Cookbook/src/test/java/chapter3/README.md
+++ Java_Cookbook/src/test/java/chapter3/README.md
@@ -1,1 +1,21 @@
 ### 字符串
+
+String是一个预定义类型，是Java的基本类型
+
+字符串是一个对象，它有自己的方法，它并不是一个字符数组
+
+字符串对象一旦构造了实例就不能改变
+
+String类有final标记，所以不能派生子类
+
+---
+
+StringTokenizer 划分字符串
+
+---
+
+StringBuilder 是一组可变的字符数组
+StringBuilder （线程不同步）   StringBuffer（线程同步）
+AbstractStringBuilder 是 StringBuilder 和 StringBuffer 的父类
+
+
--- Java_Cookbook/src/test/java/chapter3/test.java
+++ Java_Cookbook/src/test/java/chapter3/test.java
@@ -1,7 +1,44 @@
 package chapter3;
 
+import org.junit.Test;
+
+import java.util.StringTokenizer;
+
 /**
  * Created by LK on 2017/6/17.
  */
 public class test {
+    String a = "Hello word";
+
+    @Test
+    public void testSubString() {
+        String a = "asdfj;algajkasuifoq;z,nv;huta";
+        String b = a.substring(5);
+        String c = a.substring(7, 12);
+        System.out.println(a + "\b" + b + "\n" + c);
+    }
+
+    @Test
+    //StringTokenizer 按照单词边界划分字符串
+    public void testStringTokenizer() {
+        String a = "Hello everyone HAHAHA!";
+        StringTokenizer st = new StringTokenizer(a);
+        while (st.hasMoreElements()) {
+            System.out.println("Token:" + st.nextToken());
+        }
+
+        String b = "Hel|lo every|one HA|HA|HA!";
+        StringTokenizer st2 = new StringTokenizer(b, "|");
+        while (st2.hasMoreElements()) {
+            System.out.println("Token:" + st2.nextToken());
+        }
+    }
+
+    @Test
+    public void testForEachChar() {
+        for (char s : a.toCharArray()) {
+            System.out.println(s);
+        }
+    }
+
 }
